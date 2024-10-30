0965f31f43c52c115262ea79fb11d59ff1adf650
Shahin Sadik <shahinsadik@gmail.com>
2024-10-30 10:10:21 +0600

    Bike rental

--- src/redux/features/rentals/rentals.api.ts
+++ src/redux/features/rentals/rentals.api.ts
@@ -1,46 +1,47 @@
-import { baseApi } from '@/redux/api/baseApi';
+import { baseApi } from "@/redux/api/baseApi";
 
 const rentalApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     createRental: builder.mutation({
       query: (rentalInfo) => {
         return {
-          method: 'POST',
-          url: '/rentals',
+          method: "POST",
+          url: "/rentals",
           body: rentalInfo,
         };
       },
-      invalidatesTags: ['rental', 'bike'],
+      invalidatesTags: ["rental", "bike"],
     }),
     myRentals: builder.query({
       query: () => {
         return {
-          method: 'GET',
-          url: '/rentals',
+          method: "GET",
+          url: "/rentals",
         };
       },
-      providesTags: ['rental'],
+      providesTags: ["rental"],
     }),
     getAllRentals: builder.query({
       query: () => {
         return {
-          method: 'GET',
-          url: '/rentals/allRentals',
+          method: "GET",
+          url: "/rentals/allRentals",
         };
       },
-      providesTags: ['rental'],
+      providesTags: ["rental"],
     }),
     returnBike: builder.mutation({
       query: (id) => {
         return {
-          method: 'PUT',
+          method: "PUT",
           url: `/rentals/${id}/return`,
         };
       },
-      invalidatesTags: ['rental', 'bike'],
+      invalidatesTags: ["rental", "bike"],
     }),
   }),
 });
+
 
 export const {
   useCreateRentalMutation,
