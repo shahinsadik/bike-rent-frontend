cb3bd1332eccf035c1cb122c935b4149c141a42f
Shahin Sadik <shahinsadik@gmail.com>
2024-10-30 10:09:53 +0600

    Bike api solves

--- src/redux/features/bikes/bikes.api.ts
+++ src/redux/features/bikes/bikes.api.ts
@@ -1,54 +1,53 @@
-import { baseApi } from '@/redux/api/baseApi';
+import { baseApi } from "@/redux/api/baseApi";
 
 const bikeApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     createBike: builder.mutation({
       query: (bikeInfo) => {
-        
         return {
-          method: 'POST',
-          url: '/bikes',
+          method: "POST",
+          url: "/bikes",
           body: bikeInfo,
         };
       },
-      invalidatesTags: ['bike'],
+      invalidatesTags: ["bike"],
     }),
     getAllBikes: builder.query({
       query: () => {
         return {
-          method: 'GET',
-          url: '/bikes',
+          method: "GET",
+          url: "/bikes",
         };
       },
-      providesTags: ['bike', 'rental'],
+      providesTags: ["bike", "rental"],
     }),
     getSingleBike: builder.query({
       query: (id) => {
         return {
-          method: 'GET',
+          method: "GET",
           url: `/bikes/${id}`,
         };
       },
-      providesTags: ['bike', 'rental'],
+      providesTags: ["bike", "rental"],
     }),
     deleteBike: builder.mutation({
       query: (id) => {
         return {
-          method: 'DELETE',
+          method: "DELETE",
           url: `/bikes/${id}`,
         };
       },
-      invalidatesTags: ['bike'],
+      invalidatesTags: ["bike"],
     }),
     updateBikeInfo: builder.mutation({
       query: ({ id, bikeInfo }) => {
         return {
-          method: 'PUT',
+          method: "PUT",
           url: `/bikes/${id}`,
           body: bikeInfo,
         };
       },
-      invalidatesTags: ['bike'],
+      invalidatesTags: ["bike"],
     }),
   }),
 });
